a61c46b1dcac4b4e43d86bf6b95985d648e19f01
add003 <ahmedsami03@protonmail.com>
2022-01-31 12:16:37 +0100

    .

--- examples/1.hello.cpp
+++ examples/1.hello.cpp
@@ -3,8 +3,9 @@
 
 int main() {
     std::string msg = "Hello World";
+    std::string correctHash = "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e";
     auto sha256_hash = SHA2::SHA256::hash(msg);
-    std::cout << "\nInput:  " << msg << "\nSHA256: " << sha256_hash << "\n";
+    std::cout << "INPUT:  " << msg << "\nOUTPUT: " << sha256_hash << "\nCHECK:  " << correctHash;
 
     return 0;
 }
--- examples/2-abc.cpp
+++ examples/2-abc.cpp
@@ -3,8 +3,9 @@
 
 int main() {
     std::string msg = "abc";
+    std::string correctHash = "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad";
     auto sha256_hash = SHA2::SHA256::hash(msg);
-    std::cout << "\nInput:  " << msg << "\nSHA256: " << sha256_hash << "\n";
+    std::cout << "INPUT:  " << msg << "\nOUTPUT: " << sha256_hash << "\nCHECK:  " << correctHash;
 
     return 0;
 }
