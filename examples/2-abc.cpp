e0efda5dfb66b0373544b6c7749a846fc9e1f283
add003 <ahmedsami03@protonmail.com>
2022-01-31 11:55:09 +0100

    new example

---
+++ examples/2-abc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <HashLib/sha256.hpp>
+
+int main() {
+    std::string msg = "abc";
+    auto sha256_hash = SHA2::SHA256::hash(msg);
+    std::cout << "\nInput:  " << msg << "\nSHA256: " << sha256_hash;
+
+    return 0;
+}
