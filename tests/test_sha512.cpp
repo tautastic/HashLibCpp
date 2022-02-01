c6d44759ebbaea3c203e19c9737ae59296e35963
add003 <ahmedsami03@protonmail.com>
2022-02-01 09:09:17 +0100

    .

--- tests/test_sha512.cpp
+++ tests/test_sha512.cpp
@@ -7,7 +7,7 @@ using namespace SHA2::SHA512;
 int main() {
     std::array<std::string,5> in = {
         "",
-        "ab",
+        "abc",
         "Hello World",
         "Helloe3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855World",
         "7d620e4050b5715dc83e8528cfabcHelloe3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855Worldcf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a812c74fd17edafd80e8447b0d46741ee243b7eb74dd2149a0ab1b9246fb30382f27e853d8585719e0e67cbda0daa8f51671064615d645ae27acb15bfb1447f459baf927da3e"
