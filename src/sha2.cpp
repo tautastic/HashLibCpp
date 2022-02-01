b0233493a45fd5e421d3431ed57ea6dd5daff240
add003 <ahmedsami03@protonmail.com>
2022-02-01 05:47:39 +0100

    More helper functions to transform big endian to small endian and vice versa. Only declare and define u128_to_u32 if compiler is GNU or Clang

--- src/sha2.cpp
+++ src/sha2.cpp
@@ -17,15 +17,57 @@ namespace SHA2 {
     }
 
     void u32_to_u8(const uint32_t& u32, uint8_t* u8) {
-        *((u8) + 3) = (uint8_t) ((u32)      );
-        *((u8) + 2) = (uint8_t) ((u32) >>  8);
-        *((u8) + 1) = (uint8_t) ((u32) >> 16);
-        *((u8) + 0) = (uint8_t) ((u32) >> 24);
+        *(u8 + 3) = static_cast<uint8_t>(u32      );
+        *(u8 + 2) = static_cast<uint8_t>(u32 >>  8);
+        *(u8 + 1) = static_cast<uint8_t>(u32 >> 16);
+        *(u8 + 0) = static_cast<uint8_t>(u32 >> 24);
     }
     void u8_to_u32(const uint8_t* u8, uint32_t* u32) {
-        *(u32) = ((uint32_t) *((u8) + 3)      )
-               | ((uint32_t) *((u8) + 2) <<  8)
-               | ((uint32_t) *((u8) + 1) << 16)
-               | ((uint32_t) *((u8) + 0) << 24);
+        *(u32) = (static_cast<uint32_t>(*(u8 + 3))   )
+            | (static_cast<uint32_t>(*(u8 + 2)) <<  8)
+            | (static_cast<uint32_t>(*(u8 + 1)) << 16)
+            | (static_cast<uint32_t>(*(u8 + 0)) << 24);
     }
+
+
+    void u64_to_u8(const uint64_t& u64, uint8_t* u8) {
+        *(u8 + 7) = static_cast<uint8_t>(u64      );
+        *(u8 + 6) = static_cast<uint8_t>(u64 >>  8);
+        *(u8 + 5) = static_cast<uint8_t>(u64 >> 16);
+        *(u8 + 4) = static_cast<uint8_t>(u64 >> 24);
+        *(u8 + 3) = static_cast<uint8_t>(u64 >> 32);
+        *(u8 + 2) = static_cast<uint8_t>(u64 >> 40);
+        *(u8 + 1) = static_cast<uint8_t>(u64 >> 48);
+        *(u8 + 0) = static_cast<uint8_t>(u64 >> 56);
+    }
+    void u8_to_u64(const uint8_t* u8, uint64_t* u64) {
+        *(u64) = (static_cast<uint64_t>(*(u8 + 7))   )
+            | (static_cast<uint64_t>(*(u8 + 6)) <<  8)
+            | (static_cast<uint64_t>(*(u8 + 5)) << 16)
+            | (static_cast<uint64_t>(*(u8 + 4)) << 24)
+            | (static_cast<uint64_t>(*(u8 + 3)) << 32)
+            | (static_cast<uint64_t>(*(u8 + 2)) << 40)
+            | (static_cast<uint64_t>(*(u8 + 1)) << 48)
+            | (static_cast<uint64_t>(*(u8 + 0)) << 56);
+    }
+    #if defined(__clang__) || defined(__GNUC__) || defined(__GNUG__)
+    void u128_to_u8(const uint128_t& u128, uint8_t* u8) {
+        *(u8 + 15) = static_cast<uint8_t>(u128      );
+        *(u8 + 14) = static_cast<uint8_t>(u128 >>  8);
+        *(u8 + 13) = static_cast<uint8_t>(u128 >> 16);
+        *(u8 + 12) = static_cast<uint8_t>(u128 >> 24);
+        *(u8 + 11) = static_cast<uint8_t>(u128 >> 32);
+        *(u8 + 10) = static_cast<uint8_t>(u128 >> 40);
+        *(u8 + 9)  = static_cast<uint8_t>(u128 >> 48);
+        *(u8 + 8)  = static_cast<uint8_t>(u128 >> 56);
+        *(u8 + 7)  = static_cast<uint8_t>(u128 >> 64);
+        *(u8 + 6)  = static_cast<uint8_t>(u128 >> 72);
+        *(u8 + 5)  = static_cast<uint8_t>(u128 >> 80);
+        *(u8 + 4)  = static_cast<uint8_t>(u128 >> 88);
+        *(u8 + 3)  = static_cast<uint8_t>(u128 >> 96);
+        *(u8 + 2)  = static_cast<uint8_t>(u128 >> 104);
+        *(u8 + 1)  = static_cast<uint8_t>(u128 >> 112);
+        *(u8 + 0)  = static_cast<uint8_t>(u128 >> 120);
+    }
+    #endif
 }
