d8a4c1501868004447ae81ae812d6f46ba420c3c
add003 <ahmedsami03@protonmail.com>
2022-02-02 12:30:12 +0100

    Replace std::cin examples with char* argv[] examples.

--- examples/sha384_argv.cpp
+++ examples/sha384_argv.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <HashLib/sha384.hpp>
 
-int main() {
-    std::cout << "\n================SHA384================\nInput: ";
+int main(int argc, char* argv[]) {
     std::string msg;
-    std::getline(std::cin, msg);
+    for(int i = 1; i < argc; i++) {
+        msg += (i > 1) ? (" " + std::string(argv[i])) : std::string(argv[i]);
+    }
     auto sha384_hash = SHA2::SHA384::hash(msg);
-    std::cout << "OUTPUT: " << sha384_hash << "\n================SHA384================\n";
-
+    std::cout << "\n================SHA384================";
+    std::cout << "\nInput: "  << msg;
+    std::cout << "\nOutput: " << sha384_hash;
+    std::cout << "\n================SHA384================\n\n";
     return 0;
 }
--- examples/sha512_argv.cpp
+++ examples/sha512_argv.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <HashLib/sha512.hpp>
 
-int main() {
-    std::cout << "\n================SHA512================\nInput: ";
+int main(int argc, char* argv[]) {
     std::string msg;
-    std::getline(std::cin, msg);
+    for(int i = 1; i < argc; i++) {
+        msg += (i > 1) ? (" " + std::string(argv[i])) : std::string(argv[i]);
+    }
     auto sha512_hash = SHA2::SHA512::hash(msg);
-    std::cout << "OUTPUT: " << sha512_hash << "\n================SHA512================\n";
-
+    std::cout << "\n================SHA512================";
+    std::cout << "\nInput: "  << msg;
+    std::cout << "\nOutput: " << sha512_hash;
+    std::cout << "\n================SHA512================\n\n";
     return 0;
 }
